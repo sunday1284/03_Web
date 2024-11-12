fcfafdb798b1e9a28ba37ed9cf869d8b54e90e30
Your Name <sunday1284>
2024-11-12 17:46:50 +0900

    commit

---
+++ vscode/js/random.js
@@ -0,0 +1,7 @@
+
+function random(minValue,maxValue){
+    //Math.floor(Math.random() * (최대값-최소값+1)+최소값)
+   return Math.floor(Math.random()* (maxValue-minValue+1)+minValue);
+                            // (100 - 65 + 1) +65
+}
+
